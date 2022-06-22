4bf87ab5bbd0d1d11a095d4ca0a2c6a57fcf2b79
jacksontcarroll <jacksontcarroll@gmail.com>
2022-06-22 13:18:43 -0400

    2022_06_22 13:18:43

--- polybar/scripts/stream-lookup.sh
+++ polybar/scripts/stream-lookup.sh
@@ -1,13 +1,31 @@
 #!/bin/zsh
 
 API_KEY=$(cat ~/.api_keys/TWITCH_API_KEY)
-ISLIVE=$(curl -s -X GET "https://api.twitch.tv/helix/streams?user_login=$1" \
+list=$(curl -s -X GET "https://api.twitch.tv/helix/users/follows?from_id=150805300" \
 	-H "Authorization: Bearer $API_KEY" \
--H "Client-Id: euvt37d8eaujg0i06y5uk4f85nvc3n" | jq '.data | .[0] | .type')
+	-H "Client-Id: euvt37d8eaujg0i06y5uk4f85nvc3n" | jq '.data | .[] | .to_name' \
+	| sed 's/"\(.*\)"/\1/')
 
-if [[ "$ISLIVE" == "\"live\"" ]]; then
-	RED="$(xrdb -get color1)"
-	echo "%{F$RED}雷 $1 is %{u$RED +u}LIVE!%{u- F-}"
+count=0
+declare -i count
+
+for line in $list
+do
+	ISLIVE=$(curl -s -X GET "https://api.twitch.tv/helix/streams?user_login=$line" \
+		-H "Authorization: Bearer $API_KEY" \
+	-H "Client-Id: euvt37d8eaujg0i06y5uk4f85nvc3n" | jq '.data | .[0] | .type')
+
+	if [[ "$ISLIVE" == "\"live\"" ]]; then
+		count+=1
+	fi
+done
+
+RED="$(xrdb -get color1)"
+
+if [[ $count -eq 0 ]]; then
+	echo "Nobody is streaming :("
+elif [[ $count -eq 1 ]]; then
+	echo "%{F$RED}雷 $count streamer is %{u$RED +u}LIVE!%{u- F-}"
 else
-	echo "$1 is offline"
+	echo "%{F$RED}雷 $count streamers are %{u$RED +u}LIVE!%{u- F-}"
 fi
